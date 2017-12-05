cb283ef2a6a9520874cc1d8e8f8910fac57cc0d7
yurimann <yuri.manahan@gmail.com>
2017-12-05 18:28:06 -0500

    switch to before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
-  before_filter :require_login
+  before_action :require_login
 
   def current_user
     if session[:user_id]
