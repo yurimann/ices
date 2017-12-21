bb05f69a2ab03835b0f331b0965d2303a2a34846
yurimann <yuri.manahan@gmail.com>
2017-12-21 12:55:02 -0500

    add unscope to groupby

--- app/controllers/profits_controller.rb
+++ app/controllers/profits_controller.rb
@@ -9,8 +9,8 @@ class ProfitsController < ApplicationController
 
     sales = Sale.where("date >= ? AND date <= ?", range.beginning_of_month, range.end_of_month)
     expenses = Expense.where("date >= ? AND date <= ?", range.beginning_of_month, range.end_of_month)
-    weekly_sales = sales.group_by_day_of_week(:date).sum(:amount)
-    weekly_expenses = expenses.group_by_day_of_week(:date).sum(:amount)
+    weekly_sales = sales.unscope(:order).group_by_day_of_week(:date).sum(:amount)
+    weekly_expenses = expenses.unscope(:order).group_by_day_of_week(:date).sum(:amount)
 
     @weekly_profits = {}
     day = {"0": "Sunday", "1": "Monday", "2": "Tuesday", "3": "Wednesday", "4": "Thursday", "5": "Friday", "6": "Saturday"}
@@ -36,8 +36,8 @@ class ProfitsController < ApplicationController
     expenses = Expense.where("date >= ? AND date <= ?", year_start, year_end).order(:date)
 
     month = {"0": "january", "1": "february", "2": "march", "3": "april", "4": "may", "5": "june", "6": "july", "7": "august", "8": "september", "9": "october", "10": "november", "11": "december"}
-    monthly_sales = sales.group_by_month(:date, format: "%B").sum(:amount)
-    monthly_expenses = expenses.group_by_month(:date, format: "%B").sum(:amount)
+    monthly_sales = sales.unscope(:order).group_by_month(:date, format: "%B").sum(:amount)
+    monthly_expenses = expenses.unscope(:order).group_by_month(:date, format: "%B").sum(:amount)
     @monthly_profits = {}
 
     x = 0
